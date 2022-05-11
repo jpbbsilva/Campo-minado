bc9fda811f704425023be9ebd7385b946c83c49b
jpbbsilva <102251005+jpbbsilva@users.noreply.github.com>
2022-05-11 08:43:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//dificldade
+function facil() {
+    window.location.href = "facil.html"
+}
+function medio() {
+    window.location.href = "medio.html"
+}
+function blood() {
+    window.location.href = "blood.html"
+}
